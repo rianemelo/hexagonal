46ea51ecd7249b21c94e1fe9c208f1c25903388a
unknown <rianemlo@gmail.com>
2024-08-17 16:31:24 -0300

    Criado projeto no spring initalizr.

---
+++ src/test/java/br/com/sopftplan/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sopftplan.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
